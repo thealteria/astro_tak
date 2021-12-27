04790441b422bd7584879b14bc2e19bca864fcde
Aman Gupta <gaman100071@gmail.com>
2021-12-28 00:39:09 +0530

    AppImages: added images

--- lib/app/common/values/app_images.dart
+++ lib/app/common/values/app_images.dart
@@ -1,6 +1,14 @@
 class AppImages {
-  ///add app images here
   const AppImages._();
 
-  static const String icGallery = 'assets/images/ic_gallery.png';
+  static const String icAsk = 'assets/images/ask.png';
+  static const String icFilter = 'assets/images/filter.png';
+  static const String icHome = 'assets/images/home.png';
+  static const String icLogo = 'assets/images/logo.png';
+  static const String icSearch = 'assets/images/search.png';
+  static const String icSort = 'assets/images/sort.png';
+  static const String icTalk = 'assets/images/talk.png';
+  static const String icHam = 'assets/images/hamburger.png';
+  static const String icProfile = 'assets/images/profile.png';
+  static const String icReports = 'assets/images/reports.png';
 }
